8ef7c605025d1193e32102c0446230a88158ad50
RandeepSingh <s.randeep417@gmail.com>
2022-04-20 22:44:38 -0400

    Initializing Backend

---
+++ backend/routes/student.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const router = express.router()
+let studentSchema = require("../models/Student");
+
+// CREATE Student
+router.post('/create-student', (req, res, next) => {
+    
+})
